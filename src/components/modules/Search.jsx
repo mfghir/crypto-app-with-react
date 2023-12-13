c452607fa52849b0433097a2aa52fc3507847a5c
mfghir <fatemeghafari77@gmail.com>
2023-12-12 21:41:47 -0800

    fix some css issue

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -57,24 +57,26 @@ const Search = ({ currency, setCurrency }) => {
         <option value="jpy">JPY</option>
       </select>
 
-      <div className={styles.searchResult}>
-        {isLoading && (
-          <RotatingLines
-            width="50px"
-            height="50px"
-            strokeWidth="2"
-            strokeColor="#3874ff"
-          />
-        )}
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              <img src={coin.thumb} alt={coin.name} />
-              <p>{coin.name} </p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResult}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeWidth="2"
+              strokeColor="#3874ff"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name} </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
