b392fd53e0bd0dbf7bfa3a4732f412eaad54874a
mfghir <fatemeghafari77@gmail.com>
2023-12-12 21:11:15 -0800

    showing searched data

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -9,6 +9,8 @@ const Search = ({ currency, setCurrency }) => {
 
   useEffect(() => {
     const controller = new AbortController();
+    
+    setCoins([]);
     if (!text) return;
 
     const search = async () => {
@@ -45,6 +47,17 @@ const Search = ({ currency, setCurrency }) => {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+
+      <div>
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              <img src={coin.thumb} alt={coin.name} />
+              <p>{coin.name} </p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
